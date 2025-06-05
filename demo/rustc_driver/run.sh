78de5adc0d47a96b872089bbbd3488a4b1b944c0
zjp <jiping_zhou@foxmail.com>
2025-06-05 03:58:22 +0000

    fix(run.sh): remove sqlite3 cache before cargo test

--- demo/rustc_driver/run.sh
+++ demo/rustc_driver/run.sh
@@ -1,6 +1,10 @@
 set -ex
 set -o pipefail
 
+# NOTE:
+# 1. set UPDATE_EXPECT=1 to update snapshot tests
+# 2. sqlite3 cache may influence snapshots, so remove them when manually cargo test
+
 # Set up toolchain: works under current folder.
 export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib
 # Don't emit rlib files.
@@ -13,7 +17,9 @@ cargo build
 export SAFE_TOOL=$PWD/target/debug/safe-tool
 export CARGO_SAFE_TOOL=$PWD/target/debug/cargo-safe-tool
 
+rm data.sqlite3 a.sqlite3 -f
 cargo test
+rm data.sqlite3 a.sqlite3 -f
 
 pushd safety-tool-lib
 cargo test
@@ -26,6 +32,7 @@ popd
 # Test basic demo
 pushd ./tests/basic
 
+rm data.sqlite3 -f
 cargo clean
 
 # Emit artifacts for build scripts.
@@ -35,3 +42,4 @@ unset STOP_COMPILATION
 # Same as `cargo safe-tool` when tag-std and cargo-safe-tool are installed.
 CARGO_TERM_PROGRESS_WHEN=never $CARGO_SAFE_TOOL | tee macro-expanded/cargo-safe-tool.txt
 cargo expand --lib >macro-expanded/lib.rs
+rm data.sqlite3 -f
