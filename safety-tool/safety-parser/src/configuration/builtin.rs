7f6195a76f90bb1ff7ca1650c8f93299e03f3d5d
zjp <jiping_zhou@foxmail.com>
2025-11-26 15:57:02 +0000

    parser(builtin): remove CallOnce because it requires a scope; add PostToFunc

--- safety-tool/safety-parser/src/configuration/builtin.rs
+++ safety-tool/safety-parser/src/configuration/builtin.rs
@@ -3,24 +3,24 @@ use super::{Tag, TagType};
 /// `any` tag is denied in user's spec, and special in doc generation.
 pub const ANY: &str = "any";
 /// `CallOnce` tag is builtin. Users can't define it.
-pub const CALL_ONCE: &str = "CallOnce";
+pub const POST_TO_FUNC: &str = "PostToFunc";
 
 /// Returns true if the tag is builtin.
 pub fn is_builtin_tag(name: &str) -> bool {
-    matches!(name, ANY | CALL_ONCE)
+    matches!(name, ANY | POST_TO_FUNC)
 }
 
 /// Instances of builtin tags. `any` is excluded.
 pub fn tags() -> Vec<(&'static str, Tag)> {
-    vec![(CALL_ONCE, call_once())]
+    vec![(POST_TO_FUNC, post_to_func())]
 }
 
-fn call_once() -> Tag {
+fn post_to_func() -> Tag {
     Tag {
         args: Box::new(["func".into()]),
-        desc: Some("The `{func}` must be called only once.".into()),
+        desc: Some("This function must be called after {func}.".into()),
         expr: None,
-        types: Box::new([ TagType::Precond]),
-        url: Some("https://github.com/Artisan-Lab/tag-std/blob/main/Asterinas-safety-properties.md#14-calloncescope".into()),
+        types: Box::new([TagType::Precond]),
+        url: Some("https://github.com/Artisan-Lab/tag-std/blob/main/Asterinas-safety-properties.md#11-posttofuncfunc".into()),
     }
 }
